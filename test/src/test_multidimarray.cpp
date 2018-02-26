d27395894a9dbf34274af2498e1f47c489d13cb0
Nicolas HUIN <KOSA@GodLaptop.localdomain>
2018-02-25 23:31:34 -0500

    * Fixed missing braces

--- test/src/test_multidimarray.cpp
+++ test/src/test_multidimarray.cpp
@@ -6,7 +6,7 @@
 
 TEST_CASE("Test operator()", "[]") {
     const int x = 25, y = 56, z = 8;
-    MultiDimArray<int, 3> m1({x, y, z}, 5);
+    MultiDimArray<int, 3> m1({{x, y, z}}, 5);
     REQUIRE(m1.getTotalSize() == x * y * z);
     REQUIRE(m1.getDim<0>() == x);
     REQUIRE(m1.getDim<1>() == y);
@@ -18,10 +18,10 @@ TEST_CASE("Test operator()", "[]") {
 
                 if (std::make_tuple(i, j, k) != std::make_tuple(0, 1, 2)) {
                     REQUIRE(m1(i, j, k) == 5);
-                    REQUIRE(m1({i, j, k}) == 5);
+                    REQUIRE(m1({{i, j, k}}) == 5);
                 } else {
                     REQUIRE(m1(0, 1, 2) == 1);
-                    REQUIRE(m1({0, 1, 2}) == 1);
+                    REQUIRE(m1({{0, 1, 2}}) == 1);
                 }
             }
         }
@@ -32,15 +32,15 @@ TEST_CASE("Test for each loop", "[]") {
     int x = 25, y = 56, z = 8;
     MultiDimArray<int, 3> m1({{x, y, z}}, 5);
 
-    for (auto& v : MultiDimArray<int, 3>::DimensionView<2>(m1, {25, 56, 0})) {
+    for (auto& v : MultiDimArray<int, 3>::DimensionView<2>(m1, {{25, 56, 0}})) {
         v = 1;
     }
 
     for (int i = 0; i < m1.getDim<2>(); ++i) {
-        REQUIRE(m1({25, 56, i}) == 1);
+        REQUIRE(m1({{25, 56, i}}) == 1);
     }
 
-    for (const auto& v : MultiDimArray<int, 3>::DimensionView<2>(m1, {25, 56, 0})) {
+    for (const auto& v : MultiDimArray<int, 3>::DimensionView<2>(m1, {{25, 56, 0}})) {
         REQUIRE(v == 1);
     }
 }
