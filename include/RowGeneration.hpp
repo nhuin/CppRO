07116d0417ed75605c159495e371f4860e33e7ad
Nicolas HUIN <nicolasjfhuin@gmail.com>
2021-01-31 15:56:35 +0100

    + Added RowGeneration and namespace

--- include/RowGeneration.hpp
+++ include/RowGeneration.hpp
@@ -1,5 +1,5 @@
-#ifndef COLUMN_GENERATION_HPP
-#define COLUMN_GENERATION_HPP
+#ifndef ROW_GENERATION_HPP
+#define ROW_GENERATION_HPP
 
 #include <bits/c++config.h>
 #include <ilcplex/ilocplexi.h>
@@ -10,37 +10,37 @@
 
 #include <omp.h>
 
-namespace CppRO::ColumnGeneration {
+namespace CppRO::RowGeneration {
 
 /**
- * ParallelPricer manages multi-threaded pricing resolutions
+ * ParallelSeparater manages multi-threaded pricing resolutions
  * It stores multiple copies of a pricing problem class (one for each thread)
  * and stores the generated columns by these pricings.
  */
 template <typename PricingProblemType, typename PricingInputIterator>
-class ParallelPricer {
-    using ColumnType = typename PricingProblemType::ColumnType;
+class ParallelSeparater {
+    using RowType = typename PricingProblemType::RowType;
     std::vector<PricingProblemType> m_pricings;
     std::pair<PricingInputIterator, PricingInputIterator> m_inputIters;
-    std::vector<std::vector<ColumnType>> m_columns;
+    std::vector<std::vector<RowType>> m_columns;
 
   public:
     template <typename... Args>
-    explicit ParallelPricer(std::size_t _nbPricing,
+    explicit ParallelSeparater(std::size_t _nbPricing,
         std::pair<PricingInputIterator, PricingInputIterator> _inputIters,
         Args&&... _args);
 
     template <typename DualValues>
-    void generateColumnns(const DualValues& _values);
+    void generateRows(const DualValues& _values);
 
-    const std::vector<std::vector<ColumnType>>& getColumns() const {
+    const std::vector<std::vector<RowType>>& getRows() const {
         return m_columns;
     }
 };
 
 template <typename PricingProblemType, typename PricingInputIterator>
 template <typename... Args>
-ParallelPricer<PricingProblemType, PricingInputIterator>::ParallelPricer(
+ParallelSeparater<PricingProblemType, PricingInputIterator>::ParallelSeparater(
     std::size_t _nbPricing,
     std::pair<PricingInputIterator, PricingInputIterator> _inputIters,
     Args&&... _args)
@@ -59,7 +59,7 @@ ParallelPricer<PricingProblemType, PricingInputIterator>::ParallelPricer(
 
 template <typename PricingProblemType, typename PricingInputIterator>
 template <typename DualValues>
-void ParallelPricer<PricingProblemType, PricingInputIterator>::generateColumnns(
+void ParallelSeparater<PricingProblemType, PricingInputIterator>::generateRows(
     const DualValues& _dualValues) {
 #pragma omp parallel num_threads(m_pricings.size()) default(none)              \
     shared(m_pricings, _dualValues)
@@ -82,19 +82,19 @@ void ParallelPricer<PricingProblemType, PricingInputIterator>::generateColumnns(
 
 template <typename RMP, typename DualValues, typename PricingProblemType,
     typename PricingInputIterator>
-std::size_t moveColumns(RMP& _rmp,
-    ParallelPricer<PricingProblemType, PricingInputIterator>& _pricer,
+std::size_t moveRows(RMP& _rmp,
+    ParallelSeparater<PricingProblemType, PricingInputIterator>& _pricer,
     const DualValues& _values) {
-    std::size_t nbAddedColumns = 0;
-    for (const auto& cols : _pricer.getColumns()) {
+    std::size_t nbAddedRows = 0;
+    for (const auto& cols : _pricer.getRows()) {
         for (const auto& col : cols) {
-            if (_rmp.isImprovingColumn(col, _values)) {
-                _rmp.addColumn(col);
-                ++nbAddedColumns;
+            if (_rmp.isImprovingRow(col, _values)) {
+                _rmp.addRow(col);
+                ++nbAddedRows;
             }
         }
     }
-    return nbAddedColumns;
+    return nbAddedRows;
 }
 
 template <typename RMP, typename DualValues>
@@ -110,11 +110,11 @@ std::optional<DualValues> solve(const RMP& _rmp, IloCplex& _solver) {
  * _solverFunc is the function used to solve the rmp and return an std::optional
  * with the dual values.
  */
-template <typename RMP, typename SolverFunc, typename GenerateColumnFunc>
-bool solve(RMP& _rmp, SolverFunc&& _solverFunc,
-    GenerateColumnFunc&& _genColFunc, bool _verbose) {
+template <typename RMP, typename SolverFunc, typename GenerateRowFunc>
+bool solve(RMP& _rmp, SolverFunc&& _solverFunc, GenerateRowFunc&& _genRowFunc,
+    bool _verbose) {
     std::size_t nbIte = 0;
-    std::size_t nbAddedColumns = 0;
+    std::size_t nbAddedRows = 0;
     do {
         ++nbIte;
         if (_verbose) {
@@ -122,13 +122,12 @@ bool solve(RMP& _rmp, SolverFunc&& _solverFunc,
         }
         if (auto dualValues = _solverFunc(_rmp); dualValues) {
             if (_verbose) {
-                std::cout << "#cols:" << _rmp.getNbColumns() << '\n';
+                std::cout << "#cols:" << _rmp.getNbRows() << '\n';
                 std::cout << "Searching for new columns..." << std::flush;
             }
-            nbAddedColumns = _genColFunc(_rmp, *dualValues);
+            nbAddedRows = _genRowFunc(_rmp, *dualValues);
             if (_verbose) {
-                std::cout << "added " << nbAddedColumns
-                          << " new columns...\n\n";
+                std::cout << "added " << nbAddedRows << " new columns...\n\n";
             }
         } else {
             if (_verbose) {
@@ -136,9 +135,9 @@ bool solve(RMP& _rmp, SolverFunc&& _solverFunc,
             }
             return false;
         }
-    } while (nbAddedColumns > 0);
+    } while (nbAddedRows > 0);
     return true;
 }
 
-} // namespace CppRO::ColumnGeneration
+} // namespace CppRO::RowGeneration
 #endif
