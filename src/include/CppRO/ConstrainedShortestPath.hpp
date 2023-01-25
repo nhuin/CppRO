80754a1c6fd14358fa0c3860e9ff2335eed81493
Nicolas HUIN <nicolas.huin@imt-atlantique.fr>
2023-01-25 16:38:38 +0100

    + Added CSP

---
+++ src/include/CppRO/ConstrainedShortestPath.hpp
@@ -0,0 +1,89 @@
+#ifndef CPPRO_CONSTRAINEDSHORTESTPATH
+#define CPPRO_CONSTRAINEDSHORTESTPATH
+
+#include <CppRO/cplex_utility.hpp>
+#include <boost/range.hpp>
+#include <vector>
+
+namespace CppRO {
+
+class CompactConstrainedShortestPathModel {
+    IloModel m_model;
+    // Vars
+    std::vector<IloNumVar> m_flowVars;
+
+    std::vector<IloRange> m_flowConservationConstraints;
+    IloRange m_delayConstraint;
+
+  public:
+    /**
+     * Build a model according to the graph and the delay of the link
+     *
+     * \tparam Graph A class representing a graph
+     * \tparam DelayPropertyMap A class representing a property map of delays
+     **/
+    template <typename Graph, typename DelayPropertyMap>
+    CompactConstrainedShortestPathModel(IloEnv& _env, const Graph& _graph,
+        const DelayPropertyMap& _delayPropertyMap);
+
+    /**
+     * Set the source and destination of the constrained shortest path
+     */
+    void setNodePair(std::size_t _source, std::size_t _target);
+
+    template <typename DelayPropertyMap>
+    void setDelays(const DelayPropertyMap& _delayMap);
+
+    /**
+     * Returns the built model
+     */
+    [[nodiscard]] const IloModel& getModel() const { return m_model; }
+
+    /**
+     * Return the flow variables
+     */
+    [[nodiscard]] const std::vector<IloNumVar>& getFlowVars() const {
+        return m_flowVars;
+    }
+
+    void setMaxDelay(const double _maxDelay) {
+        m_delayConstraint.setUB(_maxDelay);
+    }
+};
+
+template <typename Graph, typename DelayPropertyMap>
+CompactConstrainedShortestPathModel::CompactConstrainedShortestPathModel(
+    IloEnv& _env, const Graph& _graph,
+    const DelayPropertyMap& _delayPropertyMap)
+    : m_model(_env)
+    , m_flowVars(num_edges(_graph),
+          IloAdd(m_model, IloNumVar(m_model.getEnv(), 0, 1, ILOBOOL)))
+    , m_flowConservationConstraints([&] {
+        std::vector<IloRange> retval;
+        for (const auto u : boost::make_iterator_range(vertices(_graph))) {
+            IloExpr expr(m_model.getEnv());
+            for (const auto ed :
+                boost::make_iterator_range(out_edges(u, _graph))) {
+                expr += m_flowVars[get(boost::edge_index_t, ed, _graph)];
+            }
+            for (const auto ed :
+                boost::make_iterator_range(out_edges(u, _graph))) {
+                expr -= m_flowVars[get(boost::edge_index_t, ed, _graph)];
+            }
+            retval.emplace_back(IloAdd(m_model, IloRange(expr == 0)));
+        }
+        return retval;
+    }())
+    , m_delayConstraints([&] {
+        IloExpr arcSumExpr;
+        for (const auto ed : boost::make_iterator_range(edges(_graph))) {
+            arcSumExpr += get(_delayPropertyMap, ed, _graph)
+                          * m_flowVars[get(boost::edge_index, ed, _graph)];
+        }
+        return IloAdd(m_model, IloRange(arcSum <= 0.0));
+    }())
+
+{} // namespace CppRO
+
+} // namespace CppRO
+#endif
---
+++ test/src/test_csp.cpp
@@ -0,0 +1,43 @@
+#include <CppRO/ConstrainedShortestPath.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <catch2/catch.hpp>
+
+TEST_CASE("ILP model returns optimal solution") {
+    const std::vector<std::size_t> DELAYS{50, 50, 100, 100, 60, 50, 100};
+    constexpr auto A = 0;
+    constexpr auto B = 1;
+    constexpr auto C = 2;
+    constexpr auto D = 3;
+    constexpr auto E = 4;
+    constexpr auto F = 5;
+
+    std::vector<std::pair<std::size_t, std::size_t>> edgeList{
+        {A, B}, {B, C}, {A, D}, {D, E}, {E, C}, {E, F}, {F, C}};
+
+    using DiGraph = boost::adjacency_list<boost::vecS, boost::vecS,
+        boost::bidirectionalS, boost::no_property,
+        boost::property<boost::edge_index_t, std::size_t>, boost::no_property,
+        boost::vecS>;
+    const auto NB_VERTICES = 6;
+
+    DiGraph testGraph(edgeList.begin(), edgeList.end(), NB_VERTICES);
+    auto delayMap = boost::iterator_property_map(
+        DELAYS.begin(), get(boost::edge_index, testGraph));
+
+    IloEnv env;
+    CppRO::CompactConstrainedShortestPathModel cspModel(env, testGraph, DELAYS);
+
+    IloExpr objExpression(env);
+    for (const auto ed : boost::make_iterator_range(edges(testGraph))) {
+        objExpression +=
+            1.0 * cspModel.getFlowVars()[get(boost::edge_index, testGraph, ed)];
+    }
+    IloObjective obj(env);
+    obj.setExpr(objExpression);
+    IloModel model;
+    model.add(cspModel.getModel());
+    model.add(obj);
+    IloCplex solver(model);
+    auto res = solver.solve();
+    REQUIRE(res == IloTrue);
+}
