44a53a1ccc0ed396f746b24502197a39a14f8484
Nicolas HUIN <nicolasjfhuin@gmail.com>
2023-02-06 12:29:40 +0100

    * Updated version

--- conanfile.py
+++ conanfile.py
@@ -12,7 +12,7 @@ from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
 
 class CppROConan(ConanFile):
     name = "CppRO"
-    version = "0.0.2"
+    version = "0.1.0"
 
     # Optional metadata
     license = "Mozilla Public License 2.0"
